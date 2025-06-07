c29708701c0940fac078c411b41ce21c28039041
Killian-Aidalinfo <killian.stein@aidalinfo.fr>
2025-06-08 01:31:44 +0200

    feat: update build process to accept custom registry path and remove weather agent from Mastra initialization

--- test/src/mastra/index.ts
+++ test/src/mastra/index.ts
@@ -1,10 +1,5 @@
 
 import { Mastra } from '@mastra/core';
-import { weatherAgent } from './registry/agents/weather-agent';
 
-export const mastra = new Mastra(
-    {
-        agents: { weatherAgent}
-    }
-)
+export const mastra = new Mastra()
         
